fbbdb3abbecf5fc3c50007c2b6af1a378488f07e
jennerdulce <jennerdulce@gmail.com>
2022-06-27 15:58:21 -0700

    some content

--- react/cat-tinder-test/src/App.js
+++ react/cat-tinder-test/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import Home from './pages/Home.js'
+import CatIndex from './pages/CatIndex.js'
+import CatShow from './pages/CatShow.js'
+import CatNew from './pages/CatNew.js'
+import CatEdit from './pages/CatEdit.js'
+import NotFound from './pages/NotFound.js'
+import mockCats from './mockCats.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      cats: mockCats
+    }
+  }
+
+  render() {
+    console.log(this.state.cats)
+    return (
+      <div>
+        <h1>Cat Tinder</h1>
+        <Header />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/catindex" component={CatIndex} />
+            <Route path="/catshow" component={CatShow} />
+            <Route path="/catnew" component={CatNew} />
+            <Route path="/catedit" component={CatEdit} />
+            <Route component={NotFound}/>
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
